5ff0be5d0158fb21d9ca6526d6e60aaf6a41429b
Dan <dvwhite1981@gmail.com>
2023-08-20 18:15:01 -0400

    Complete

---
+++ sql-practice/practice.sql
@@ -0,0 +1,37 @@
+.read seed-data.sql
+
+/* Directions say "Amy Pond", but it is
+   "Ryan Pond" in the table
+*/
+UPDATE friends
+SET first_name = 'Amy',
+    last_name = 'Blue'
+WHERE first_name = 'Ryan'
+AND last_name = 'Pond';
+/* If it was "Amy Pond" :
+UPDATE friends
+SET last_name = 'Blue'
+WHERE first_name = 'Amy'
+AND last_name = 'Pond';
+*/
+
+UPDATE friends
+SET last_name = 'Smith'
+WHERE first_name = 'Tyler'
+  AND last_name = 'Rose';
+/* This doesn't do anything */
+
+/* Directions say "Rose Tyler", but it is
+   "Sky Tyler" in the table
+*/
+UPDATE friends
+SET first_name = 'Rose',
+    last_name = 'Smith'
+WHERE first_name = 'Sky'
+AND last_name = 'Tyler';
+/* If it was "Rose Tyler" :
+UPDATE friends
+SET last_name = 'Smith'
+WHERE first_name = 'Rose'
+AND last_name = 'Tyler';
+*/
